6450fcdc5cced50a856c1147836f701ff2183a7a
MAC <MAC@MacBook-Pro-3.local>
2025-03-11 01:10:01 +0700

    up

--- backend/config/vcenterConfig.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-const configPath = path.join(__dirname, '../data/vcenter-config.json');
-const configData = fs.readFileSync(configPath, 'utf8');
-const vCenterConfig = JSON.parse(configData);
-
-module.exports = { vCenterConfig };
--- frontend/src/hooks/useAnsible.js
+++
@@ -1,1 +0,0 @@
-// This file is no longer needed as Ansible is not used anymore.
